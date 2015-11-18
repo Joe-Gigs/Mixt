43d4408fe1a2a15dfc291205752a37a314f99a15
gigs <jgiglio86@gmail.com>
2015-11-18 15:33:57 -0500

    first major merge

--- app/models/tape.rb
+++ app/models/tape.rb
@@ -1,8 +1,9 @@
 class Tape < ActiveRecord::Base
 	belongs_to :user
+	# has_many :songs
 
 
-has_attached_file :song
+ has_attached_file :song
 validates_attachment_presence :song
 validates_attachment_content_type :song, :content_type => [ 'audio/mp3','audio/mpeg']
 end

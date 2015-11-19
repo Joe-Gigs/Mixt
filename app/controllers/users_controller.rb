5b86bd11b461264b61bcc13e13fd1c1aa023ef58
Jose Plata <jose.a.plata@gmail.com>
2015-11-19 10:18:34 -0500

    Profile styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
-		redirect_to log_in_path
+		redirect_to @user
 	else
 		render :new
 	end

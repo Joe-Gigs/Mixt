8d98b7c3e6e7fb6dbdf0230e9f102180ba732a00
gigs <jgiglio86@gmail.com>
2015-11-19 10:13:48 -0500

    user can see tapes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,11 @@ class SessionsController < ApplicationController
     @user = User.find_by_username(params[:username])
     if @user.save
       session[:user_id] = @user.id
+      puts "**session created**"
       flash[:notice] = "Success."
     else
       flash[:alert] = "There was a problem"
+      puts "**oh fuck**"
     end
       redirect_to @user
   end
@@ -18,6 +20,7 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     flash[:notice] = "Signed out."
     redirect_to root_path
+    puts "session terminated"
   end
 
 end

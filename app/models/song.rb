489c265d1ecd90d3c74dca492fc23ca86ab7c280
Jose Plata <jose.a.plata@gmail.com>
2015-11-23 01:19:09 -0500

    Missing the song to play

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,13 @@
 class Song < ActiveRecord::Base
      belongs_to :tape
-	 has_attached_file :song
+     belongs_to :user, class_name: :user, foreign_key: :user_id
+	 has_attached_file :song,
+	 					:path => ":rails_root/app/assets/audios/:filename",
+	 					:url  => "/songs/:id/:filename",
+	 					:default_url => "/assets/audios"
+
 	 validates_attachment_presence :song
 	 validates_attachment_content_type :song, :content_type => [ 'audio/mp3','audio/mpeg']
+	 validates_attachment_size :song, :less_than => 10.megabytes, 
+	    :message => 'filesize must be less than 10 MegaBytes' 
 end

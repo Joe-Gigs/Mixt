4ca323a32414ebfbc71d86a723c7af8ee2fe3025
Jose Plata <jose.a.plata@gmail.com>
2015-12-15 10:27:09 -0500

    Corrected indentation, got rid of empty files...

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,15 @@
 class SessionsController < ApplicationController
-
   def new
-    
   end
 
   def create
     @user = User.find_by(username: params[:username])
       if @user && @user.password == params[:password]
         session[:user_id] = @user.id
-        puts "**session created**"
         flash[:notice] = "Success."
         redirect_to @user
       else
         flash[:alert] = "Wrong credentials. Please try again"
-        puts "**oh fuck**"
         render :new
       end
   end
@@ -22,7 +18,6 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     flash[:notice] = "Signed out."
     redirect_to root_path
-    puts "**session terminated**"
   end
 
 end
